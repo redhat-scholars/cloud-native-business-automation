1646459cde746a45b020e94fe48423b6df24509d
Donato Marrazzo <donato.marrazzo@gmail.com>
2021-09-09 18:34:03 +0200

    first kogito intro and process lab

--- site.sh
+++ site.sh
@@ -3,4 +3,7 @@
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 rm -rf $_CURR_DIR/gh-pages $_CURR_DIR/.cache
 
-antora --pull --stacktrace  site.yml
+#antora --pull --stacktrace  site.yml
+
+podman run -v $PWD:/antora:Z --rm -it antora/antora antora generate --stacktrace site.yml
+podman run -v ./gh-pages/:/var/www/html/:Z --rm -it -p 3000:8080 registry.access.redhat.com/rhscl/httpd-24-rhel7
